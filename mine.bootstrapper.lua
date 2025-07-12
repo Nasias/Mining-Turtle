3962f5e69d50df4db264cb6c43913128c6e6cfac
Nasias Darkstar <n@siasdarkstar.com>
2025-07-12 17:03:29 +0100

    Configure bootstrapper. Remove install code from main file.

--- mine.bootstrapper.lua
+++ mine.bootstrapper.lua
@@ -1,15 +1,74 @@
-local mainFileUrl = "https://raw.githubusercontent.com/Nasias/Mining-Turtle/refs/heads/main/mine.lua"
+local args = {...}
 
-print("Downloading main file from: " .. mainFileUrl)
-local response, error, failedResponse  = http.get(mainFileUrl)
-if not response then
-   error("Failed to fetch main file: " .. (error or "Unknown error"))
+local INSTALL_REPO_URL = "https://raw.githubusercontent.com/Nasias/Mining-Turtle/refs/heads/main/"
+local INSTALL_FILE_MANIFEST_URL = INSTALL_REPO_URL + "install_manifest"
+local DEFAULT_INSTALL_PATH = "/mine/"
+
+local function fetchInstallManifest(installManifestUrl)
+   print("Downloading install manifest from: " .. installManifestUrl)
+
+   local response, error, failedResponse = http.get(installManifestUrl)
+   if not response then
+      error("Failed to fetch install manifest: " .. (error or "Unknown error"))
+   end
+
+   local manifestContent = response.readAll()
+   response.close()
+   
+   return manifestContent:replace("\r", ""):split("\n\r")
 end
 
-print("Saving " .. mainFileUrl)
-local fileHandle = fs.open("mine.lua", "w+")
-fileHandle.write(response.readAll())
-fileHandle.close()
-response.close()
+local function downloadFileAsync(remoteFileUrlToDownload, localFilePathToSaveTo)
+   print("Downloading " .. remoteFileUrlToDownload)
 
-shell.run("mine.lua", "--install")
+   http.request(remoteFileUrlToDownload)
+
+   while true do
+      local eventType, eventUrl, eventHttpHandle = os.pullEvent();
+      if eventType == "http_success" and eventUrl == remoteFileUrlToDownload then
+         print(string.format("Saving %s to %s\n ", remoteFileUrlToDownload, localFilePathToSaveTo))
+         local fileHandle = fs.open(localFilePathToSaveTo, "w+")
+         fileHandle.write(eventHttpHandle.readAll())
+         fileHandle.close()
+         eventHttpHandle.close()
+         print(string.format("Saved %s to %s\n ", remoteFileUrlToDownload, localFilePathToSaveTo))
+         return true
+      elseif eventType == "http_failure" and eventUrl == remoteFileUrlToDownload then
+         print(string.format("Failed to fetch file %s", remoteFileUrlToDownload))
+         return false
+      end
+   end
+end
+
+local function installFiles(installPath, installRepoUrl, installFileManifestUrl)
+   local files = fetchInstallManifest(installFileManifestUrl)
+   local tasks = {}
+   local errorCount = 0
+
+   for _, fileName in ipairs(files) do
+      local requestUrl = installRepoUrl .. fileName
+      tasks[#tasks + 1] = function()
+         if not downloadFileAsync(requestUrl, fileName) then
+            errorCount = errorCount + 1
+         end
+      end
+   end
+
+   parallel.waitForAll(table.unpack(tasks))
+
+   if errorCount > 0 then
+      error(string.format("Failed to download %d files", errorCount))
+   else
+      print("All files downloaded")
+   end
+end
+
+local installPath
+if not args or #args == 0 then
+   installPath = DEFAULT_INSTALL_PATH
+elseif args[1] == "--install-path" and args[2] ~= nil then
+   installPath = args[2]
+end
+
+print("--- mine.boostrapper ---\n")
+installFiles(installPath, INSTALL_REPO_URL, INSTALL_FILE_MANIFEST_URL)
