5f57ff284e3ffbf2ca69865c3e8f00a35c6b86d5
Nasias Darkstar <n@siasdarkstar.com>
2025-07-12 14:57:26 +0100

    first commit

---
+++ mine.lua
@@ -0,0 +1,16 @@
+local args = {...}
+
+local inifile = require("lib.inifile")
+local config = inifile.parse("config.ini")
+
+print("Config loaded:")
+for section, keys in pairs(config) do
+   print(string.format("[%s]", section))
+   for key, value in pairs(keys) do
+      if type(value) == "table" and value.x ~= nil and value.y ~= nil and value.z ~= nil then
+         print(string.format("%s=%d|%d|%d)", value.x, value.y, value.z))
+      else
+         print(string.format("%s=%s", key, value))
+      end
+   end
+end
